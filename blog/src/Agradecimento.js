6f1b54d650fef56403942f05fb2d73ac2b3ae795
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-16 16:08:11 -0300

    Bug1
    Arrumado o bug1 de inicialização da database, que estava impedindo o roteamento das paginas

--- blog/src/Agradecimento.js
+++ blog/src/Agradecimento.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import Footer from './footer';
+import Header from './header';
+import Form from './form';
 
 class Obrigado extends Component{
     render(){
         return(
-            <div className="agradecimento">
-                Obrigado pela inscrição
+            <div>    
+                <Header/>
+                <Form/>
+                <div className="agradecimento">
+                    Obrigado pela inscrição
+                </div>
+                <Footer/>
             </div>
         )
     }
--- blog/src/form.js
+++ blog/src/form.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 
 import $ from 'jquery';
 import Firebase from "firebase";
@@ -7,139 +6,143 @@ import config from "./config";
 
 class Form extends Component {
     constructor(props) {
-        super(props);
+      super(props);
+      if (!Firebase.apps.length) {
         Firebase.initializeApp(config);
-    
-        this.state = {
-          inscritos: []
-        };
-      }
-    
-      componentDidMount() {
-        console.log("DidMount");
-        this.getUserData();
-      }
-    
-      componentDidUpdate(prevProps, prevState) {
-        console.log("DidUpdate");
-        // Verificar o acrécimos de dados
-        if (prevState !== this.state) {
-          this.writeUserData()
-        }
-      }
-    
-      writeUserData = () => {
-        Firebase.database()
-          .ref("/")
-          .set(this.state);
-        console.log("DATA SAVED");
+    }
+      this.state = {
+        inscritos: []
       };
+    }
     
-      getUserData = () => {
-        console.log("getUserData");
-        let ref = Firebase.database().ref("/");
-        ref.on("value", snapshot => {
-          const state = snapshot.val();
-          this.setState(state);
+    componentDidMount() {
+      console.log("DidMount");
+      this.getUserData();
+    }
+  
+    componentDidUpdate(prevProps, prevState) {
+      console.log("DidUpdate");
+      // Verificar o acrécimos de dados
+      if (prevState !== this.state) {
+        this.writeUserData()
+      }
+    }
+  
+    writeUserData = () => {
+      Firebase.database()
+        .ref("/")
+        .set(this.state);
+      console.log("DATA SAVED");
+    };
+  
+    getUserData = () => {
+      console.log("getUserData");
+      let ref = Firebase.database().ref("/");
+      ref.on("value", snapshot => {
+        const state = snapshot.val();
+        this.setState(state);
+      });
+    }; 
+  
+    formatoData(d, h){
+      return (
+        d.getFullYear()+'-'+('00'+(d.getMonth()+1)).slice(-2)+'-'+('00'+d.getDate()).slice(-2)+' '+('00'+(d.getHours()+h)).slice(-2)+':'+('00'+d.getMinutes()).slice(-2)+':'+('00'+d.getSeconds()).slice(-2)
+      )
+    }
+  
+    resetCampo(){
+      this.refs.nome.value = "";
+      this.refs.email.value = "";
+      this.refs.uid.value = "";
+      console.log('Reset');
+    }
+  
+    salvandoJson(uid, nome, email, tipo, data, ip){
+      console.log("Salvando JSON");
+      // para encontrar alguma inscricao antiga
+      if (uid && nome && email && tipo) {
+        const { inscritos } = this.state;
+        const insIndex = inscritos.findIndex(data => {
+          return data.uid === uid;
         });
-      }; 
-    
-      formatoData(d, h){
-        return (
-          d.getFullYear()+'-'+('00'+(d.getMonth()+1)).slice(-2)+'-'+('00'+d.getDate()).slice(-2)+' '+('00'+(d.getHours()+h)).slice(-2)+':'+('00'+d.getMinutes()).slice(-2)+':'+('00'+d.getSeconds()).slice(-2)
-        )
+        inscritos[insIndex].nome = nome;
+        inscritos[insIndex].email = email;
+        inscritos[insIndex].tipo = tipo;
+        inscritos[insIndex].data = data;
+        inscritos[insIndex].ip = ip;
+        this.setState({ inscritos });
+      } //Caso for inscrição nova, segue
+        else if (nome && email && tipo) {
+        const uid = new Date().getTime().toString();
+        const { inscritos } = this.state;
+        inscritos.push({ uid, nome, email, tipo, data, ip });
+        this.setState({ inscritos });
       }
-    
-      resetCampo(){
-        this.refs.nome.value = "";
-        this.refs.email.value = "";
-        this.refs.uid.value = "";
-        console.log('Reset');
-      }
-    
-      salvandoJson(uid, nome, email, tipo, data, ip){
-        // para encontrar alguma inscricao antiga
-        if (uid && nome && email && tipo) {
-          const { inscritos } = this.state;
-          const insIndex = inscritos.findIndex(data => {
-            return data.uid === uid;
-          });
-          inscritos[insIndex].nome = nome;
-          inscritos[insIndex].email = email;
-          inscritos[insIndex].tipo = tipo;
-          inscritos[insIndex].data = data;
-          inscritos[insIndex].ip = ip;
-          this.setState({ inscritos });
-        } //Caso for inscrição nova, segue
-          else if (nome && email && tipo) {
-          const uid = new Date().getTime().toString();
-          const { inscritos } = this.state;
-          inscritos.push({ uid, nome, email, tipo, data, ip });
-          this.setState({ inscritos });
+    }
+  
+    handleSubmit = event => {
+      console.log("HandleSubmit");
+      event.preventDefault();
+      let nome = this.refs.nome.value;
+      let email = this.refs.email.value;
+      let tipo = $('input[name=tipo]:checked',"#tipo").val();
+      let ipapi = null;
+      $.ajax({
+        url: 'https://ipapi.co/json/',
+        type: 'get',
+        async: false,
+        success: function(dados){
+          ipapi = dados;
         }
-      }
-    
-      handleSubmit = event => {
-        event.preventDefault();
-        let nome = this.refs.nome.value;
-        let email = this.refs.email.value;
-        let tipo = $('input[name=tipo]:checked',"#tipo").val();
-        let ipapi = null;
-        $.ajax({
-          url: 'https://ipapi.co/json/',
-          type: 'get',
-          async: false,
-          success: function(dados){
-            ipapi = dados;
-          }
-        });
-        let fuso = ipapi.utc_offset;
-        let ip = ipapi.ip;
-        // Cálculo da diferença de fuso com o fuso de brasília
-        var diffHora = ((parseInt(fuso)/100)+3);
-        let data = this.formatoData(new Date(), diffHora);
-        let uid = this.refs.uid.value;
-        console.log(uid);
-    
-        //chamada de função para salvar o JSON que será enviado para a DB
-        this.salvandoJson(uid, nome, email, tipo, data, ip);
-        // aplicar regra no db.rules, para arrumar a ordem
-        this.resetCampo();
-        // Implementar a limpeza do campo de radio
-      };
+      });
+      let fuso = ipapi.utc_offset;
+      let ip = ipapi.ip;
+      // Cálculo da diferença de fuso com o fuso de brasília
+      var diffHora = ((parseInt(fuso)/100)+3);
+      let data = this.formatoData(new Date(), diffHora);
+      let uid = this.refs.uid.value;
+      console.log(uid);
+  
+      //chamada de função para salvar o JSON que será enviado para a DB
+      this.salvandoJson(uid, nome, email, tipo, data, ip);
+      // aplicar regra no db.rules, para arrumar a ordem
+      this.resetCampo();
+      // Implementar a limpeza do campo de radio
+    };
     render(){
-        return(   
-            <form onSubmit={this.handleSubmit} className="subscribe">
-            <input type="hidden" ref="uid" />
-            <input
-                type="text"
-                ref="nome"
-                placeholder="Nome*"
-                className="campoform"
-            />
-            <input
-            type="email"
-            ref="email"
-            placeholder="E-mail*"
-            className="campoform"
-            />
-            <div id="tipo">
-              <div>
-                <input type="radio" name="tipo" value="B2B"/>
-                  <label>Pessoal</label>
-              </div>
-              <div>
-                <input type="radio" name="tipo" value="B2C"/>
-                <label>Comercial</label>
-              </div>
+      console.log("Renderizado");
+      return(   
+          <form onSubmit={this.handleSubmit} className="subscribe">
+          <input type="hidden" ref="uid" />
+          <input
+              type="text"
+              ref="nome"
+              placeholder="Nome*"
+              className="campoform"
+          />
+          <input
+          type="email"
+          ref="email"
+          placeholder="E-mail*"
+          className="campoform"
+          />
+          <div id="tipo">
+            <div>
+              <input type="radio" name="tipo" value="B2B"/>
+                <label>Pessoal</label>
             </div>
-            <Link to="/obrigado">
-              <button type="submit" className="botao">
-                Inscreva-se
-              </button>
-            </Link>
-          </form>
-        )
+            <div>
+              <input type="radio" name="tipo" value="B2C"/>
+              <label>Comercial</label>
+            </div>
+          </div>
+          {/* <Link to="/obrigado"> */}
+            <button type="submit" className="botao">
+              Inscreva-se
+            </button>
+          {/* </Link> */}
+        </form>
+      )
     }
 }
 
