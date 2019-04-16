15d44c3af3015f25ebe58e3a379910af2d5e2f5d
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-16 00:11:35 -0300

    resume
    criação do resume, e adição do primeiro texto

---
+++ blog/src/resume.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Resume extends Component {
+    render(){
+        return(
+            <div className="container">
+                <div className="foto">FOTO</div>
+                <div className="resume1">
+                    <h1>Declaração Anual do MEI</h1>
+                    <p>Se você é Microempreendedor Individual (MEI), ou seja, fatura até R$ 81 mil por ano, você tem compromisso anual com a Declaração Anual do Simples Nacional (DASN) e deixar para a última hora a entrega pode ser um grande problema. Entenda porquê.</p>
+                </div>
+            </div>
+        )
+    }
+}
+export default Resume;
