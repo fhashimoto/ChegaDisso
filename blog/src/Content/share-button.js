53acf22212ceb41b44bf240b3b06006768d082f4
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 10:14:47 -0300

    share buttons
    adicionando os botoes de share do linkedin, facebook e twitter

--- blog/src/Content/share-button.js
+++ blog/src/Content/share-button.js
@@ -4,10 +4,18 @@ class Share extends Component {
     render(){
         let url = window.location.href;
         return(
-            <div className="fb-share-button" data-href={url} data-layout="button" data-size="small">
-                <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} className="fb-xfbml-parse-ignore" rel="noopener noreferrer">
-                    Share
+            <div>
+                <div className="fb-share-button" data-href={url} data-layout="button" data-size="small">
+                    <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} className="fb-xfbml-parse-ignore" rel="noopener noreferrer">
+                        Share
+                    </a>
+                </div>
+                <a href={`https://twitter.com/share?ref_src=${url}`} className="twitter-share-button" data-show-count="false">
+                    Tweet
                 </a>
+                <div className="a2a_kit a2a_default_style">
+                    <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`} className="a2a_button_linkedin_share"></a>
+                </div>
             </div>
         )
     }
