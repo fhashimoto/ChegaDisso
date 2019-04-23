9e05af06c48d67a02e542805313100ce83d35bc6
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 11:00:30 -0300

    Update footer.js
    tirando o botao de inscreva-se do footer

--- blog/src/footer.js
+++ blog/src/footer.js
@@ -2,13 +2,9 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Footer extends Component {
-    topo(){
-        window.scrollTo(0,0);
-    }
     render(){
         return(
             <div>
-                <button id="botaoFooter" onClick={this.topo}>Inscreva-se</button>
                 <div className="footer">
                     <Link to="/" className="linkColorTituloFooter">
                         <div className="titulo" onClick={this.topo}>Chega disso</div>
