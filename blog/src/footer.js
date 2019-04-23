e12203590fb677bb07f8b5658ec686de3d44540f
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 13:56:05 -0300

    footer
    atualizando o footer

--- blog/src/footer.js
+++ blog/src/footer.js
@@ -9,13 +9,13 @@ class Footer extends Component {
                     <Link to="/" className="linkColorTituloFooter">
                         <div className="titulo" onClick={this.topo}>Chega disso</div>
                     </Link>
-                    <div className="info">
+                    {/* <div className="info">
                         <Link to="/about" className="linkColorFooter">
                             <span>Sobre</span>
                         </Link>
                         <span>Equipe</span>
                         <span>Contato</span>
-                    </div>
+                    </div> */}
                     <div className="copyr"> Copyright &copy; - Chega Disso - Todos os direitos reservados</div>
                 </div>
             </div>
