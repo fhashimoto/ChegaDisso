ed068179adc06975709f822e8c59568fc053797a
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 11:17:19 -0300

    share buttons
    alterando os botoes de share e utilizando a API do add to any

--- blog/src/Content/share-button.js
+++ blog/src/Content/share-button.js
@@ -2,22 +2,15 @@ import React, {Component} from 'react';
 
 class Share extends Component {
     render(){
-        let url = window.location.href;
         return(
+            // Utilizando a API do Add to Any, para os botões de compartilhamento
             <div>
-                <div className="fb-share-button" data-href={url} data-layout="button" data-size="small">
-                    <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} className="fb-xfbml-parse-ignore" rel="noopener noreferrer">
-                        Share
-                    </a>
-                </div>
-                <a href={`https://twitter.com/share?ref_src=${url}`} className="twitter-share-button" data-show-count="false">
-                    Tweet
-                </a>
-                <div className="a2a_kit a2a_default_style">
-                    <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`} className="a2a_button_linkedin_share"></a>
-                </div>
-                <div className="a2a_kit a2a_kit_size_20 a2a_default_style">
+                <div className="a2a_kit a2a_kit_size_32 a2a_default_style">
+                    <a className="a2a_button_facebook"></a>
                     <a className="a2a_button_whatsapp"></a>
+                    <a className="a2a_button_linkedin"></a>
+                    <a className="a2a_button_email"></a>
+                    <a className="a2a_button_twitter"></a>
                 </div>
             </div>
         )
