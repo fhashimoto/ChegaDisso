5003aadc7b6d2a48110a049b228b97705d6aadbc
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 13:45:01 -0300

    Update share-button.js
    colocando o alt da img e o rel do anchor

--- blog/src/Content/share-button.js
+++ blog/src/Content/share-button.js
@@ -6,17 +6,17 @@ class Share extends Component {
         return(
             // Utilizando a API do Add to Any, para os botões de compartilhamento
             <div className="share">
-                <a className="facebook" target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}>
-                    <img src="https://i.imgur.com/SgHnbfV.png"></img>
+                <a className="facebook" target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/SgHnbfV.png" alt="compartilhe no facebook"></img>
                 </a>
-                <a className="linkedin" target="_blank" href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}>
-                    <img src="https://i.imgur.com/kX3EGMw.png"></img>
+                <a className="linkedin" target="_blank" href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`} rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/kX3EGMw.png" alt="compartilhe no linkedin"></img>
                 </a>
-                <a className="twitter" target="_blank" href={`https://twitter.com/share?ref_src=${url}`}>
-                    <img src="https://i.imgur.com/dcDvS3s.png"></img>
+                <a className="twitter" target="_blank" href={`https://twitter.com/share?ref_src=${url}`} rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/dcDvS3s.png" alt="compartilhe no twitter"></img>
                 </a>
-                <a className="whatsapp" target="_blank" href={`https://api.whatsapp.com/send?phone=whatsappphonenumber&text=${url}`}>
-                    <img src="https://i.imgur.com/zAjSZC3.png"></img>
+                <a className="whatsapp" target="_blank" href={`https://api.whatsapp.com/send?phone=whatsappphonenumber&text=${url}`} rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/zAjSZC3.png" alt="compartilhe no whatsapp"></img>
                 </a>
             </div>
         )
