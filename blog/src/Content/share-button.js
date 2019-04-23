7c55a7af1aea63091846ab0e585c812465eb5dba
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-23 10:22:18 -0300

    Update share-button.js
    botao do whatsapp

--- blog/src/Content/share-button.js
+++ blog/src/Content/share-button.js
@@ -16,6 +16,9 @@ class Share extends Component {
                 <div className="a2a_kit a2a_default_style">
                     <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`} className="a2a_button_linkedin_share"></a>
                 </div>
+                <div className="a2a_kit a2a_kit_size_20 a2a_default_style">
+                    <a className="a2a_button_whatsapp"></a>
+                </div>
             </div>
         )
     }
